8a182ed7e2864619b41bf43aa77ce4fa20680140
Tirthankar Chakravarty <tirthankar.chakravarty@gmail.com>
2015-03-16 12:53:16 +0530

    Simple SEM example with dot file export.

---
+++ Code/14-Advanced-dplyr.R
@@ -0,0 +1,12 @@
+#====================================================================
+# purpose: dplyr tutorial (new features in 0.3 and 0.4)
+# author: tirthankar chakravarty
+# created: 16th march 2015
+# revised:
+# comments: this is based on the dataschool.io video tutorial:
+# https://www.youtube.com/watch?v=2mh1PqfsXVI
+#====================================================================
+
+library(nycflights13)
+library(dplyr)
+
